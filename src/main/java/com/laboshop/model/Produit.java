0e6f2d8dc27c275f8016e28943b92e179cab927e
meganefenyo@gmail.copm <tagnemegane@gmail.com>
2026-08-21 12:34:16 +0000

    premiere modification

--- src/main/java/com/laboshop/model/Produit.java
+++ src/main/java/com/laboshop/model/Produit.java
@@ -5,8 +5,9 @@ import java.time.LocalDate;
 public class Produit implements Ivendable{
     /**contient le suffixe de reference de produit*/
     private static final String SUFFIXE="PRD";
+
     /** contient le suffixe des references de produit*/
-    private static final int nombreTotalDeProduit=0;
+    private static int nombreTotalDeProduit=0;
 
     private String reference;
     private String libelle;
@@ -17,8 +18,37 @@ public class Produit implements Ivendable{
     public Produit(double prixUnitaire, String libelle, String reference, LocalDate dateAjout) {
         this.prixUnitaire = prixUnitaire;
         this.libelle = libelle;
-        this.reference = reference;
         this.dateAjout = dateAjout;
+        dateAjout=LocalDate.now();
+        nombreTotalDeProduit++;
+        reference=SUFFIXE+String.format("%05d",nombreTotalDeProduit);
+
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+    public static int getNombreTotalDeProduit(){
+        return nombreTotalDeProduit;
+    }
+    protected void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public LocalDate getDateAjout() {
+        return dateAjout;
+    }
+
+    public int getQuantiteEnStock() {
+        return quantiteEnStock;
+    }
+
+    public void setQuantiteEnStock(int quantiteEnStock) {
+        this.quantiteEnStock = quantiteEnStock;
     }
 
     public double getPrixUnitaire() {
@@ -29,24 +59,16 @@ public class Produit implements Ivendable{
         this.prixUnitaire = prixUnitaire;
     }
 
-    public String getLibelle() {
-        return libelle;
-    }
-
-    public void setLibelle(String libelle) {
-        this.libelle = libelle;
-    }
-
-    protected void setReference(String reference) {
-        this.reference = reference;
-    }
-
+    /**
+     * retire de la quantite en stock le nombre de produit vendu.
+     * @param quantite
+     */
     @Override
     public void vendre(int quantite){
       if (quantite<=0){
           System.out.println("la quantite ne peut etre negatif ou null");
       }quantiteEnStock-=quantite;
-      journaliser("vendre" );
+      journaliser("vendre", quantite );
       }
 
     private void journaliser(String vendre) {
